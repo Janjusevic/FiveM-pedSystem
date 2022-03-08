db71cf9299d2989d92efede31cb157b7b339ffd4
Janjusevic011 <91884372+Janjusevic@users.noreply.github.com>
2022-03-08 17:11:42 +0100

    sql fix

--- tabela.sql
+++ tabela.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ped_system` (
-	`identifier` VARCHAR(50) NOT NULL,
-	`IME` VARCHAR(50) NOT NULL,
-	`ped` VARCHAR(50) NOT NULL
+	`identifier` VARCHAR(50) NULL,
+	`IME` VARCHAR(50) NULL,
+	`hash` VARCHAR(50) NULL
 	
 );
